bcb86f520a6fd46bc57f99018ed3a1afaba8ae38
Md. Muzhirul Islam <43285@wgbd.com>
2024-01-22 14:59:38 +0600

    feat: add material dashboard

---
+++ product/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['code','name','created_at']
+    save_on_top = True
+
+    class Meta:
+        model = Product
+
+admin.site.register(Product,ProductAdmin)
---
+++ product/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Product Name')
+    code = models.CharField(max_length=50,blank=True, null=True,verbose_name='Product Code')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'pro_product'
+
+    def __str__(self):
+        return str(self.name)
